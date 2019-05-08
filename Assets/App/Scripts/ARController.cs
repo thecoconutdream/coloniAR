1e00d2f25287cf2b998f959bf02b2c1c914bba47
Lisa-Philine Tsakiris <lphiline@web.de>
2019-05-08 15:31:24 +0200

    Add logic for portal usage:

---
+++ Assets/App/Scripts/ARController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ARController : MonoBehaviour
+{
+    public GameObject DetectedPlanePrefab;
+
+    public Camera FirstPersonCamera;
+
+    private bool m_IsQuitting = false;
+
+    public GameObject Portal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        // If the player has not touched the screen, we are done with this update.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            //Let's now place the portal on top of the tracked plane that we touched
+
+            //Enable the portal
+            Portal.SetActive(true);
+
+            //Create a new Anchor
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+            //Set the position of the portal to be the same as the hit position
+            Portal.transform.position = hit.Pose.position;
+            Portal.transform.rotation = hit.Pose.rotation;
+
+            //We want the portal to face the camera
+            Vector3 cameraPosition = FirstPersonCamera.transform.position;
+
+            //The portal should only rotate around the Y axis
+            cameraPosition.y = hit.Pose.position.y;
+
+            //Rotate the portal to face the camera
+            Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+
+            //ARCore will keep understanding the world and update the anchors accordingly hence we need to attach our portal to the anchor
+            Portal.transform.parent = anchor.transform;
+
+        }
+
+
+
+    }
+
+    /// Check and update the application lifecycle.
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to
+        // appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage(
+                "ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// Actually quit the application.
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    /// Show an Android toast message.
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity =
+            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject =
+                    toastClass.CallStatic<AndroidJavaObject>(
+                        "makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
---
+++ Assets/App/Scripts/Homescreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Homescreen : MonoBehaviour
+{
+    public GameObject hafenstrasse;
+    public GameObject roemehafen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if there is a touch
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // Check if finger is over a UI element 
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                if(EventSystem.current.currentSelectedGameObject == (hafenstrasse || roemehafen))
+                {
+                    SceneManager.LoadScene("Test");
+                }
+                Debug.Log("UI is touched");
+                //so when the user touched the UI(buttons) call your UI methods 
+            }
+        }
+
+    }
+}
